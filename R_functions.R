a03368ce8f829363d057438790719c241dba872a
dbaets <commits.dbaets@users.noreply.github.com>
2020-06-23 14:38:13 +0200

    update project structure

--- R_functions.R
+++ R_functions.R
@@ -6,3 +6,19 @@ f_load_xlsx <- function(path, sheet){
   t_input <- read_xlsx(path = path,sheet = sheet)
   return(t_input)
 }
+
+
+f_selectdf <- function(vakcijfer){
+  t_input <- NA
+  if (vakcijfer == 1){
+    t_input <- t_fys
+  } else if(vakcijfer == 2){
+    t_input <- t_che
+  } else if(vakcijfer == 3){
+    t_input <- t_bio
+  } else if(vakcijfer == 4){
+    t_input <- t_nwe
+  } else{
+    print("Vak selectie is onjuist. Dit vak is niet geladen.")
+  }
+}
